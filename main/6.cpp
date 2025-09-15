f614b1b1e5c4b19605eb5d48f1009497c2fd5831
ulyana <eroshenkovaulyana@gmail.com>
2025-09-16 00:06:25 +0300

    new changes

--- main/6.cpp
+++ main/6.cpp
@@ -1,8 +1,9 @@
+#include <iostream>
 int main() {
 	setlocale(LC_ALL, "RU");
 	int n;
 	std::cout << "¬ведите количество n: ";
-	std::cin >> n;#include <iostream>
+	std::cin >> n;
 
 	int a = 1;
 	int sum = 0;
